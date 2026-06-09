a8ac1d3f2ed4f8c1e8b6a995dc880b5cacaa6fec
Vladimír Borek <xborek@fi.muni.cz>
2026-06-09 21:46:54 +0200

    Add SurfaceType DAO

---
+++ src/main/java/com/example/tennis_club/dao/SurfaceTypeDao.java
@@ -0,0 +1,26 @@
+package com.example.tennis_club.dao;
+
+import com.example.tennis_club.entities.SurfaceType;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class SurfaceTypeDao extends BaseDao<SurfaceType> {
+
+    public SurfaceTypeDao() {
+        super(SurfaceType.class);
+    }
+
+    public Optional<SurfaceType> findActiveByName(String name) {
+        return entityManager.createQuery("""
+                        select surfaceType
+                        from SurfaceType surfaceType
+                        where lower(surfaceType.name) = lower(:name)
+                          and surfaceType.deleted = false
+                        """, SurfaceType.class)
+                .setParameter("name", name)
+                .getResultStream()
+                .findFirst();
+    }
+}
