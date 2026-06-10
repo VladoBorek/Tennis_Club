42dede1503b3fd351b2c368da8ecb2ea1b109862
Vladimír Borek <xborek@fi.muni.cz>
2026-06-10 18:13:24 +0200

    Add surfaceType mapper and dtos

---
+++ src/main/java/com/example/tennis_club/mappers/SurfaceTypeMapper.java
@@ -0,0 +1,15 @@
+package com.example.tennis_club.mappers;
+
+import com.example.tennis_club.dtos.surface.SurfaceTypeResponse;
+import com.example.tennis_club.entities.SurfaceType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SurfaceTypeMapper {
+    public static SurfaceTypeResponse toSurfaceTypeResponse(SurfaceType surfaceType) {
+        return new SurfaceTypeResponse(
+                surfaceType.getId(),
+                surfaceType.getName(),
+                surfaceType.getPricePerMinute());
+    }
+}
