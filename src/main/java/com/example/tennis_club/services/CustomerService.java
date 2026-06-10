2f560306b1b6bf6d83bdca40b245ce0a03fc971a
Vladimír Borek <xborek@fi.muni.cz>
2026-06-10 19:54:49 +0200

    Fix typo

--- src/main/java/com/example/tennis_club/services/CustomerService.java
+++ src/main/java/com/example/tennis_club/services/CustomerService.java
@@ -39,7 +39,7 @@ public class CustomerService {
 
     private Customer validateExistingCustomerName(Customer customer, String requestedName) {
         if (!customer.getName().equals(requestedName)) {
-            throw new BadRequestException("Customer is not correct for given phon number");
+            throw new BadRequestException("Customer is not correct for given phone number");
         }
 
         return customer;
