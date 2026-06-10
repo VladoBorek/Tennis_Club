310868bf2f7564cb891b878a26f785073b9b54e0
Vladimír Borek <xborek@fi.muni.cz>
2026-06-10 17:23:04 +0200

    Finish CustomerService impl

--- src/main/java/com/example/tennis_club/services/CustomerService.java
+++ src/main/java/com/example/tennis_club/services/CustomerService.java
@@ -1,4 +1,55 @@
 package com.example.tennis_club.services;
 
+import com.example.tennis_club.daos.CustomerDao;
+import com.example.tennis_club.entities.Customer;
+import com.example.tennis_club.exceptions.BadRequestException;
+import com.example.tennis_club.exceptions.NotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+// This is just simpler internal service so there is no need for dtos in my opinion to reduce boilerplate.
+
+@Service
+@Transactional
 public class CustomerService {
+    private final CustomerDao customerDao;
+
+    public CustomerService(CustomerDao customerDao) {
+        this.customerDao = customerDao;
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Customer> findActiveCustomerByPhoneNumber(String phoneNumber) {
+        return customerDao.findActiveByPhoneNumber(phoneNumber);
+    }
+
+    @Transactional(readOnly = true)
+    public Customer getActiveCustomerByPhoneNumber(String phoneNumber) {
+        return findActiveCustomerByPhoneNumber(phoneNumber)
+                .orElseThrow(() -> new NotFoundException("Customer not found"));
+    }
+
+    public Customer resolveCustomer(String phoneNumber, String name) {
+        return findActiveCustomerByPhoneNumber(phoneNumber)
+                .map(existingCustomer -> validateExistingCustomerName(existingCustomer, name))
+                .orElseGet(() -> createCustomer(phoneNumber, name));
+    }
+
+    private Customer validateExistingCustomerName(Customer customer, String requestedName) {
+        if (!customer.getName().equals(requestedName)) {
+            throw new BadRequestException("Customer is not correct for given phon number");
+        }
+
+        return customer;
+    }
+
+    private Customer createCustomer(String phoneNumber, String name) {
+        Customer customer = new Customer();
+        customer.setPhoneNumber(phoneNumber);
+        customer.setName(name);
+
+        return customerDao.save(customer);
+    }
 }
